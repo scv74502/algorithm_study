b9fdb3b260c8a1e130532d92fe58167796b72a95
kunwoo Park <scv74502@gmail.com>
2021-10-24 17:00:23 +0900

    baekjoon 12865(dp)

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -1,21 +1,21 @@
 import sys
 
-n, k = map(int, sys.stdin.readline().split())
+num, lim = map(int, sys.stdin.readline().split())
 things = []
-dp = [[0 for k in range(k)] for n in range (n)]
-for _ in range(n):
-    wei, val = map(int, sys.stdin.readline().split())
-    things.append([wei, val])
 
-print(dp)
+# 가방의 최대 무게 + 1 크기의 열(가로), 물건의 숫자만큼의 행
+dp = [[0 for lim in range(lim+1)] for num in range(num)]
+for _ in range(num):
+    w, v = map(int, sys.stdin.readline().split())
+    things.append([w, v])
 
-for i in range(0, n):
-    weight = things[i][0]
-    value = things[i][1]
-    for j in range(k):
-        if(j < things[i][0]):
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+# print(dp)
 
-print(dp)
+# 가방에 물건을 넣거나, 혹은 넣지 않거나
+# DP[i][j]=max((DP[i][j-해당차례 물건 무게]+해당차례 물건 가치),DP[i-1][j]) (배낭에 해당차례 물건을 넣거나, 넣지 않거나)
+for i in range(0, num):
+    weight, value = things[i][0], things[i][1]
+    for j in range(0, lim+1):
+        if(j < things[i][0]): dp[i][j] = dp[i-1][j]
+        else: dp[i][j] = max(dp[i-1][j], dp[i-1][j - weight] + value)
+print(dp[num-1][lim])
